26df2eea50b5c9679d615f372911dd80e0a12333
Shuah Khan <skhan@linuxfoundation.org>
2021-02-26 17:06:59 -0700

    lockdep: Add lockdep lock state defines
    Adds defines for lock state returns from lock_is_held_type() based on
    Johannes Berg's suggestions as it make it easier to read and maintain
    the lock states. These are defines and a enum to avoid changes to
    lock_is_held_type() and lockdep_is_held() return types.
    
    Updates to lock_is_held_type() and  __lock_is_held() to use the new
    defines.
    
    Signed-off-by: Shuah Khan <skhan@linuxfoundation.org>
    Signed-off-by: Peter Zijlstra (Intel) <peterz@infradead.org>
    Signed-off-by: Ingo Molnar <mingo@kernel.org>
    Link: https://lore.kernel.org/linux-wireless/871rdmu9z9.fsf@codeaurora.org/
    Signed-off-by: kvsnr113 <kvsnrprojkt113@gmail.com>

--- kernel/locking/lockdep.c
+++ kernel/locking/lockdep.c
@@ -50,6 +50,12 @@
 #include <linux/random.h>
 #include <linux/jhash.h>
 #include <linux/nmi.h>
+<<<<<<< HEAD
+=======
+#include <linux/rcupdate.h>
+#include <linux/kprobes.h>
+#include <linux/lockdep.h>
+>>>>>>> 1b251ed3225ed (lockdep: Add lockdep lock state defines)
 
 #include <asm/sections.h>
 #include <asm/stacktrace.h>
@@ -3981,14 +3987,19 @@ static int __lock_is_held(const struct lockdep_map *lock, int read)
 		struct held_lock *hlock = curr->held_locks + i;
 
 		if (match_held_lock(hlock, lock)) {
+<<<<<<< HEAD
 			if (read == -1 || hlock->read == read)
 				return 1;
+=======
+			if (read == -1 || !!hlock->read == read)
+				return LOCK_STATE_HELD;
+>>>>>>> 1b251ed3225ed (lockdep: Add lockdep lock state defines)
 
-			return 0;
+			return LOCK_STATE_NOT_HELD;
 		}
 	}
 
-	return 0;
+	return LOCK_STATE_NOT_HELD;
 }
 
 static struct pin_cookie __lock_pin_lock(struct lockdep_map *lock)
@@ -4198,14 +4209,14 @@ EXPORT_SYMBOL_GPL(lock_release);
 int lock_is_held_type(const struct lockdep_map *lock, int read)
 {
 	unsigned long flags;
-	int ret = 0;
+	int ret = LOCK_STATE_NOT_HELD;
 
 	/*
 	 * Avoid false negative lockdep_assert_held() and
 	 * lockdep_assert_not_held().
 	 */
 	if (unlikely(current->lockdep_recursion))
-		return -1;
+		return LOCK_STATE_UNKNOWN;
 
 	raw_local_irq_save(flags);
 	check_flags(flags);
