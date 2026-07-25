a0ab25a668bb77fed11e702ea8433151a6264b97
olzhas0986 <kdiroljas@gmail.com>
2026-07-25 10:35:20 +0500

    Revert "BACKPORT: epoll: wire up syscall epoll_pwait2"
    This reverts commit 531077065d789ecb0f9aa5cf7d8f8b590128e402.

--- kernel/sys_ni.c
+++ kernel/sys_ni.c
@@ -63,8 +63,6 @@ COND_SYSCALL(epoll_create1);
 COND_SYSCALL(epoll_ctl);
 COND_SYSCALL(epoll_pwait);
 COND_SYSCALL_COMPAT(epoll_pwait);
-COND_SYSCALL(epoll_pwait2);
-COND_SYSCALL_COMPAT(epoll_pwait2);
 
 /* fs/fcntl.c */
 
